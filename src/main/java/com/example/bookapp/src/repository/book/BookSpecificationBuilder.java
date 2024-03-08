e6018e6b490f00f279b517cdff68ea68496f9be5
Pavlo Polezhay <pasha200417@gmail.com>
2024-03-08 11:27:05 +0200

    checkstyle fixes

--- src/main/java/com/example/bookapp/src/repository/book/BookSpecificationBuilder.java
+++ src/main/java/com/example/bookapp/src/repository/book/BookSpecificationBuilder.java
@@ -16,14 +16,14 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
     @Override
     public Specification<Book> build(BookSearchParameters searchParameters) {
         Specification<Book> spec = Specification.where(null);
-            if (searchParameters.title() != null && searchParameters.title().length > 0) {
-                spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("title")
-                        .getSpecification(searchParameters.title()));
-            }
-            if (searchParameters.author() != null && searchParameters.author().length > 0) {
-                spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("author")
-                        .getSpecification(searchParameters.author()));
-            }
+        if (searchParameters.title() != null && searchParameters.title().length > 0) {
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("title")
+                    .getSpecification(searchParameters.title()));
+        }
+        if (searchParameters.author() != null && searchParameters.author().length > 0) {
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("author")
+                    .getSpecification(searchParameters.author()));
+        }
 
         return spec;
     }
