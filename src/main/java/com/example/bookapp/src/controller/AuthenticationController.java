798759979c61e02d8401be96bb94406c87b69490
Pavlo Polezhay <pasha200417@gmail.com>
2024-04-01 13:29:38 +0300

    added Pagination and Swagger for new controllers, fix public-available methods, added Role protection for Category controllers methods

--- src/main/java/com/example/bookapp/src/controller/AuthenticationController.java
+++ src/main/java/com/example/bookapp/src/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.example.bookapp.src.dto.user.UserResponseDto;
 import com.example.bookapp.src.exception.RegistrationException;
 import com.example.bookapp.src.security.AuthenticationService;
 import com.example.bookapp.src.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Users management", description = "Endpoints for managing")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
@@ -22,12 +25,14 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
+    @Operation(summary = "Add new user", description = "Add new user")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Login user", description = "Login user")
     public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto)
             throws RegistrationException {
         return authenticationService.authenticate(requestDto);
--- src/main/java/com/example/bookapp/src/repository/category/CategoryRepository.java
+++ src/main/java/com/example/bookapp/src/repository/category/CategoryRepository.java
@@ -2,9 +2,12 @@ package com.example.bookapp.src.repository.category;
 
 import com.example.bookapp.src.model.Category;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Page<Category> findAll(Pageable pageable);
 
     Optional<Category> findById(Long id);
 }
